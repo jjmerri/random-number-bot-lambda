054d2255082f12349c656241b73338bfbb418864
Joshua Merritt <jjmerri88@gmail.com>
2023-04-06 17:19:31 -0400

    add mentionId -jjm

--- src/random_number_bot.py
+++ src/random_number_bot.py
@@ -148,7 +148,7 @@ def process_mention(mention):
         # could be a normal mention not a command so just return
         return
 
-    request = get_rdo_request(num_randoms, num_slots)
+    request = get_rdo_request(num_randoms, num_slots, mention.id)
 
     responseData = {}
     try:
@@ -198,10 +198,10 @@ def process_mention(mention):
             logger.exception("Unknown error sending dev pm or email")
 
 
-def get_rdo_request(num_randoms, num_slots):
+def get_rdo_request(num_randoms, num_slots, mentionId):
     return {'jsonrpc': '2.0', 'method': 'generateSignedIntegers',
             'params': {'apiKey': RANDOM_ORG_API_KEY, 'n': num_randoms, 'min': 1, 'max': num_slots,
-                       'replacement': False},
+                       'replacement': False, 'userData': {'mentionId': mentionId}},
             'id': uuid.uuid4().hex}
 
 
